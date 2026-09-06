9a22c4cde9a4516b0c6fd2f71b1fc8440cd7081d
Liam Ralph <liamralph@tutamail.com>
2026-09-05 21:35:03 -0400

    add settings loading, start working on ssh

--- src/vm-manager.py
+++ src/vm-manager.py
@@ -12,7 +12,14 @@
 
 # Standard Library
 
+import os
+import shutil
 import subprocess
+
+# Third Party
+
+import keyring
+import paramiko
 
 # PySide6
 
@@ -24,9 +31,27 @@ from PySide6.QtWidgets import (
 )
 
 
-# MainWindow Class
+# Global Variables
+
+RELEASE_PATHS = False
+if RELEASE_PATHS:
+    xdg_config_home = os.getenv("XDG_CONFIG_HOME")
+    if xdg_config_home != None:
+        SETTINGS_PATH = os.path.expanduser(xdg_config_home + "/vm-manager.conf")
+    else:
+        SETTINGS_PATH = os.path.expanduser("~/.config/vm-manager.conf")
+else:
+    SETTINGS_PATH = os.path.abspath("conf/defaults.conf")
+    # Assuming program is run from project root, not /src
+
+
+# Classes
+
+# MainWindow
 
 class MainWindow(QMainWindow):
+
+    # Constructor
 
     def __init__(self):
 
@@ -35,7 +60,7 @@ class MainWindow(QMainWindow):
         # Setup MainWindow
 
         self.setWindowTitle("VM Manager")
-        # self.setWIndowIcon()
+        # self.setWindowIcon()
         self.showMaximized()
         self.setMinimumSize(600, 400)
 
@@ -44,6 +69,37 @@ class MainWindow(QMainWindow):
         window = QWidget()
         layout_back = QVBoxLayout(window)
         self.setCentralWidget(window)
+
+        # Left (Settings)
+
+        if not os.path.exists(SETTINGS_PATH):
+            shutil.copy("/usr/share/vm-manager/defaults.conf", SETTINGS_PATH)
+        settings = self.load_settings()
+
+        # Middle
+
+        # Right (VM Sizes)
+
+    # Functions
+
+    def load_settings(self):
+        """
+        Load user settings from SETTINGS_PATH.
+
+        :return: A dictionary of settings of form {setting, value}, both str.
+        """
+        settings = {}
+        with open(SETTINGS_PATH, "r") as file:
+            for line in file:
+                if (line.startswith("#") or line == "\n"):
+                    continue
+                line = line.strip()
+                if line.endswith("="):
+                    settings[line[:-1]] = ""
+                    continue
+                setting, value = line.split("=")
+                settings[setting] = value
+        return settings
 
 
 # Main Function
